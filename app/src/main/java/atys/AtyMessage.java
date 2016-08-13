437a23bc4a8f009be2b253cb6711591cf9cb6af6
crazy-Q <qmx754@163.com>
2016-08-13 15:47:46 +0800

    项目消息界面设计

--- app/src/main/java/atys/AtyMessage.java
+++ app/src/main/java/atys/AtyMessage.java
@@ -1,19 +1,18 @@
 package atys;
 
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import com.jikexueyuan.secret.R;
 
 /**
  * Created by QQQ on 2016/8/13.
  */
-public class AtyTimeLine extends AppCompatActivity {
+public class AtyMessage extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.aty_timeline);
-
+        setContentView(R.layout.aty_message);
     }
 }
