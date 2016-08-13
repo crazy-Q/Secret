0f8a7aa1fd4306bdffd10e9d690b921d13469c77
crazy-Q <qmx754@163.com>
2016-08-13 15:55:45 +0800

    项目发布消息界面设计

---
+++ app/src/main/java/atys/AtyPubMessage.java
@@ -0,0 +1,18 @@
+package atys;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jikexueyuan.secret.R;
+
+/**
+ * Created by QQQ on 2016/8/13.
+ */
+public class AtyPubMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aty_pubmessage);
+    }
+}
